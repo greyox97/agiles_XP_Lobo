713084b62c8c0526ff4dee11b9e8f237dc50d654
paulr10463 <paul.roman@epn.edu.ec>
2023-12-25 20:35:55 -0500

    feat: HU1 completed

---
+++ 03Coding/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Game game = new Game();
+        Scanner scanner = new Scanner(System.in);
+        String elementToMove;
+        while(!game.isGameOver){
+            game.printState();
+            System.out.print("Ingrese el elemento a transportar: ");
+            elementToMove = scanner.next();
+            try{
+                game.makeMove(elementToMove);
+            }
+            catch (Error e){
+                System.out.println(e.getMessage());
+            }
+        }
+        System.out.println("############## GAME OVER #####################");
+    }
+}
